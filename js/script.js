a3fd4044f6310128fb9f08a5cc92e06462ce85a3
lalbari <lalbari98@gmail.com>
2024-10-31 11:21:11 +0100

    buscador

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
   var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
   myModal.show();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Inicializa el popover
+  var popoverTriggerEl = document.getElementById('infoButton');
+  var popover = new bootstrap.Popover(popoverTriggerEl, {
+      html: true,
+      content: function () {
+          return document.getElementById('popover-content').innerHTML;
+      },
+      trigger: 'click', // Se abrirá al hacer clic
+  });
+});
